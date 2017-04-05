38040d327a1628c7f19bc3c11f41e0dc957bef9a
Claudia De Leon <claulg2701@gmail.com>
2017-04-05 01:28:01 -0400

    adding footer content

--- footer.php
+++ footer.php
@@ -1,15 +1,24 @@
       <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
-      <footer class="row">
-        <div class="four columns">
-          <?php dynamic_sidebar('footer-widget-one'); ?>
+      <footer class="footer row">
+        <div class="logo twelve columns">
+
         </div>
-        <div class="four columns">
-          <?php dynamic_sidebar('footer-widget-two'); ?>
+        <div class="search twelve columns">
+          <?php
+          wp_nav_menu(array(
+          'sort_column' => 'menu_order',
+          'container_class' => 'menu-header'
+          ));?>
         </div>
-        <div class="four columns">
-          <?php dynamic_sidebar('footer-widget-three'); ?>
-        </div>
+        <p id="copyright"><p>Coppyright 2017 &copy; | BnB Story Telling Photography</p>
       </footer>
+      <div class="social twelve columns">
+        <ul>
+          <li class="facebook"><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+    			<li class="instagram"><a href="#"><i class="fa fa-instagram fa-lg"></i></a></li>
+    			<li class="twitter"><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+        </ul>
+      </div>
     <?php wp_footer(); ?>
   </body>
 </html>
