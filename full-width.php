e8f09abfc631985dd17d45f64eb9697f4845c07e
Claudia De Leon <claulg2701@gmail.com>
2017-04-09 14:00:47 -0400

    working on slider for multiple pages

--- full-width.php
+++ full-width.php
@@ -7,5 +7,38 @@ get_header(); ?>
 	<div class="twelve columns">
 			<h2>Homepage</h2>
 	</div>
+
+  <!--begin unslider-->
+  <div class="twelve columns unslider-banner">
+    <ul>
+      <?php
+          $args   = array( 'post_type' => 'Slider' );
+          $slides = new WP_Query( $args );
+
+          if( $slides->have_posts() ) {
+            while( $slides->have_posts() ) {
+              $slides->the_post();
+
+              /*--- Build Thumbnail URL ---*/
+              $thumb_id        = get_post_thumbnail_id();
+              $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+              $thumb_url       = $thumb_url_array[0];
+              ?>
+        <li style="background-image: url('<?php echo $thumb_url ?>');" class="slide-container">
+            <div class="slides-message">
+                <h1><?php the_title() ?></h1>
+                <p><?php the_excerpt() ?></p>
+            </div>
+        </li>
+      <?php
+    }
+  }
+  else {
+    echo 'No Slides';
+  }
+?>
+  </ul>
+</div> <!-- end unslider -->
+
 </div>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -75,4 +75,38 @@ function enqueue_unslider() {
     wp_enqueue_script( 'unslider', get_template_directory_uri() . '/js/unslider.js', array('jquery'), '', true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_unslider');
+
+/*---------- Unslider Custom Post -------------*/
+function slider_unslider() {
+
+  $labels = array(
+    'name'               => _x( 'Slides', 'post type general name' ),
+    'singular_name'      => _x( 'Slide', 'post type singular name' ),
+    'add_new'            => _x( 'Add New', 'slides' ),
+    'add_new_item'       => __( 'Add Slide' ),
+    'edit_item'          => __( 'Edit Slides' ),
+    'new_item'           => __( 'New Slide' ),
+    'all_items'          => __( 'All Slides' ),
+    'view_item'          => __( 'View Slides' ),
+    'search_items'       => __( 'Search Slides' ),
+    'not_found'          => __( 'No slides found' ),
+    'not_found_in_trash' => __( 'No slides found in the Trash' ),
+    'parent_item_colon'  => '',
+    'menu_name'          => 'Slider'
+  );
+
+  $args = array(
+    /*--- Begin Arguments Options ---*/
+    'labels'        => $labels,
+    'description'   => 'Slides for our Unslider integration',
+    'public'        => true,
+    'menu_position' => 6,
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt'),
+    'has_archive'   => true,
+  );
+
+  register_post_type( 'slider', $args );
+}
+
+add_action( 'init', 'slider_unslider' );
 ?>
