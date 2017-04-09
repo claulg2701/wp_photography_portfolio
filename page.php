d5b384992e92e2aa468287786227db55650a5772
Claudia De Leon <claulg2701@gmail.com>
2017-04-09 14:44:52 -0400

    make featured image bgd div for pages

--- page.php
+++ page.php
@@ -1,21 +1,22 @@
 <?php get_header(); ?>
 	<section class="row">
+    <?php
+    if ( has_post_thumbnail() ) {
+      $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+      <div class="featured img twelve columns" style="background-image: url('<?php echo $thumb['0'];?>')">
+        <h2><?php the_title(); ?></h2>
+      </div>
+    <?php
+  }else {?>
+    <h2><?php the_title(); ?></h2>
+  <?php }?>
 		<div class="twelve columns">
-      <!-- Begin page PHP-->
+      <!-- Begin page content-->
       <?php
-      /*---- Set page featured image---*/
-      if ( has_post_thumbnail() ) {
-        the_post_thumbnail();
-      }
-
-      /* Posts loop */
-			 if (have_posts()) :
-				/* data context	*/
-				while (have_posts()) : the_post(); ?>
-					<h2><?php the_title(); ?></h2>
-					<?php the_content();
-				endwhile;
-			endif; ?>
+      if ( have_posts() ) {
+            the_post();
+            the_content();
+      } ?>
       <!-- End page PHP -->
 		</div>
 
@@ -24,7 +25,5 @@
   		<?php get_sidebar(); ?>
   	</div>
     <!-- End sidebar -->
-
 	</section>
-
 <?php get_footer(); ?>
